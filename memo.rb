5ded0b1d02ced5a73c8b002986c89e50b2744a31
nagano <shionaga0214@gmail.com>
2025-03-30 01:13:12 +0900

    メモ作成ページ

--- memo.rb
+++ memo.rb
@@ -10,9 +10,17 @@ def get_memos(file_path)
   File.open(file_path) { |f| JSON.parse(f.read) }
 end
 
+def save_memos(file_path, memos)
+  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
+end
+
 get '/memos' do
   @memos = get_memos(FILE_PATH)
   erb :index
+end
+
+get '/memos/new' do
+  erb :new
 end
 
 get '/memos/:id' do
@@ -20,4 +28,16 @@ get '/memos/:id' do
   @title = memos[params[:id]]['title']
   @content = memos[params[:id]]['content']
   erb :show
+end
+
+post '/memos' do
+  title = params[:title]
+  content = params[:content]
+
+  memos = get_memos(FILE_PATH)
+  id = (memos.keys.map(&:to_i).max + 1).to_s
+  memos[id] = { 'title' => title, 'content' => content }
+  save_memos(FILE_PATH, memos)
+
+  redirect '/memos'
 end
